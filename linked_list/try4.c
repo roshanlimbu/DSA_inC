b86a50a226eca115ad0fad99986be24e96d51d42
Roshan Limbu <topazkayle@gmail.com>
2023-07-22 21:21:19 +0545

    createNode function

--- linked_list/try4.c
+++ linked_list/try4.c
@@ -8,3 +8,9 @@ typedef struct Node {
   int data;
   struct Node *next;
 } Node;
+void *createNode(int data){
+  Node *newNode =  (Node *)malloc(sizeof(Node));
+  newNode->data=data; 
+  newNode->next=NULL;
+  return newNode;
+}
