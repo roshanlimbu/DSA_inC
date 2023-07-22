dc83a2906ecb44e4da7df67880beb668521f1d4a
Roshan Limbu <topazkayle@gmail.com>
2023-07-22 21:47:29 +0545

    described the createNode function

--- linked_list/try4.c
+++ linked_list/try4.c
@@ -9,8 +9,8 @@ typedef struct Node {
   struct Node *next;
 } Node;
 void *createNode(int data){
-  Node *newNode =  (Node *)malloc(sizeof(Node));
-  newNode->data=data; 
-  newNode->next=NULL;
-  return newNode;
+  Node *newNode =  (Node *)malloc(sizeof(Node)); // allocating memory for a new node using `malloc` func, which dynamically allocates  memeory on the heap
+  newNode->data=data; // initializes the `data` field of the newly created node with the value provided as the function argument.
+  newNode->next=NULL; // sets the `next` pointer of the node to `NULL`, assuming it's the last node in the list or will be connected to another node later
+  return newNode;// Fianlly, it returns the pointer to the newly created node
 }
