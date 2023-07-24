4d2b1edcceb62a76ab0a9e87a0fa8dddd0e68e71
Roshan Limbu <topazkayle@gmail.com>
2023-07-24 21:41:58 +0545

    initialized the stack

---
+++ try7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define MAX 10
+
+typedef struct Stack {
+  int data[MAX];
+  int top;
+} Stack;
+
+void initialize(Stack *stack){
+  stack->top= -1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
