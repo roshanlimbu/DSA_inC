7c0b64529de7f4a0bec06168c630412e4796c0ce
Roshan Limbu <topazkayle@gmail.com>
2023-07-28 21:54:05 +0545

    singly linked list

--- linked_list/try3.c
+++ linked_list/try3.c
@@ -1,106 +1,198 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
 
-// defining the structure for the nodes 
-typedef struct Node {
-  int data;
-  struct Node *next;
-} Node;
+struct Node {
+    int data;
+    struct Node* next;
+};
 
+typedef struct Node Node;
 
-// func to create the new node using malloc
-Node *createNode(int data){
-  Node *newNode = (Node *)malloc(sizeof(Node));
-  newNode->data=data; // this puts the new data to the new node
-  newNode->next=NULL; // this puts the new node pointer to null as it would be at last
-  return newNode;
-} 
+struct LinkedList {
+    Node* head;
+};
 
+typedef struct LinkedList LinkedList;
 
-// func to insert a node at the beginning of the list
-void insertNode(Node **head, int data){
-  Node *newNode = createNode(data);
-  newNode->next = *head;
-  *head = newNode;
+void insertAtFront(LinkedList* list, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = list->head;
+    list->head = newNode;
 }
 
+void insertAtLast(LinkedList* list, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
 
-void removeNode(Node **head, int data) {
-  Node *current = *head;
-  Node *prev = NULL;
+    if (list->head == NULL) {
+        list->head = newNode;
+    } else {
+        Node* current = list->head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
 
-  // If the node to be removed is the head node
-  if (current != NULL && current->data == data) {
-    *head = current->next;
+void insertAfter(LinkedList* list, int key, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+
+    if (list->head == NULL) {
+        printf("Error: Linked List is empty.\n");
+        return;
+    }
+
+    Node* current = list->head;
+    while (current != NULL && current->data != key) {
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        printf("Error: Key not found in the Linked List.\n");
+        return;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
+void insertBefore(LinkedList* list, int key, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+
+    if (list->head == NULL) {
+        printf("Error: Linked List is empty.\n");
+        return;
+    }
+
+    if (list->head->data == key) {
+        newNode->next = list->head;
+        list->head = newNode;
+        return;
+    }
+
+    Node* current = list->head;
+    while (current->next != NULL && current->next->data != key) {
+        current = current->next;
+    }
+
+    if (current->next == NULL) {
+        printf("Error: Key not found in the Linked List.\n");
+        return;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
+void removeFromList(LinkedList* list, int key) {
+    if (list->head == NULL) {
+        printf("Error: Linked List is empty.\n");
+        return;
+    }
+
+    Node* current = list->head;
+    Node* prev = NULL;
+
+    if (current->data == key) {
+        list->head = current->next;
+        free(current);
+        printf("Element with key %d removed from the list.\n", key);
+        return;
+    }
+
+    while (current != NULL && current->data != key) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        printf("Error: Key not found in the Linked List.\n");
+        return;
+    }
+
+    prev->next = current->next;
     free(current);
-    return;
-  }
-
-  // Search for the node to be removed
-  while (current != NULL && current->data != data) {
-    prev = current;
-    current = current->next;
-  }
-
-  // If the node is not found
-  if (current == NULL) {
-    printf("Node with data %d not found.\n", data);
-    return;
-  }
-
-  // Remove the node from the list
-  prev->next = current->next;
-
-  // Check if the node to be removed is the last node
-  if (current->next == NULL) {
-    // Update the head pointer if the last node is removed
-    *head = prev;
-  }
-
-  free(current);
+    printf("Element with key %d removed from the list.\n", key);
 }
 
-// void removeNode(Node **head, int data) {
-//   Node *current = *head;
-//   Node *prev = NULL;
+void displayList(LinkedList* list) {
+    Node* current = list->head;
+    if (current == NULL) {
+        printf("Linked List is empty.\n");
+        return;
+    }
+    printf("Linked List: ");
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
 
-//   // If the node to be removed is the head node
-//   if (current != NULL && current->data == data) {
-//     *head = current->next;
-//     free(current);
-//     return;
-//   }
+int main() {
+    LinkedList list;
+    list.head = NULL;
 
-//   // Search for the node to be removed
-//   while (current != NULL && current->data != data) {
-//     prev = current;
-//     current = current->next;
-//   }
+    int choice, data, key;
+    while (1) {
+        printf("1. Insert at Front\n");
+        printf("2. Insert at Last\n");
+        printf("3. Insert After an Item\n");
+        printf("4. Insert Before an Item\n");
+        printf("5. Remove an Item\n");
+        printf("6. Display List\n");
+        printf("7. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-//   // If the node is not found
-//   if (current == NULL) {
-//     printf("Node with data %d not found.\n", data);
-//     return;
-//   }
+        switch (choice) {
+            case 1:
+                printf("Enter the data to insert: ");
+                scanf("%d", &data);
+                insertAtFront(&list, data);
+                printf("Element inserted at the front.\n");
+                break;
+            case 2:
+                printf("Enter the data to insert: ");
+                scanf("%d", &data);
+                insertAtLast(&list, data);
+                printf("Element inserted at the last.\n");
+                break;
+            case 3:
+                printf("Enter the key after which to insert: ");
+                scanf("%d", &key);
+                printf("Enter the data to insert: ");
+                scanf("%d", &data);
+                insertAfter(&list, key, data);
+                break;
+            case 4:
+                printf("Enter the key before which to insert: ");
+                scanf("%d", &key);
+                printf("Enter the data to insert: ");
+                scanf("%d", &data);
+                insertBefore(&list, key, data);
+                break;
+            case 5:
+                printf("Enter the key to remove: ");
+                scanf("%d", &key);
+                removeFromList(&list, key);
+                break;
+            case 6:
+                displayList(&list);
+                break;
+            case 7:
+                printf("Exiting the program.\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+        printf("\n");
+    }
 
-//   // Remove the node from the list
-//   prev->next = current->next;
-//   free(current);
-// }
-
-// Function to display the linked list
-void displayList(Node *head) {
-  Node *current = head;
-  if (current == NULL) {
-    printf("List is empty.\n");
-    return;
-  }
-
-  printf("Linked List: ");
-  while (current != NULL) {
-    printf("%d ", current->data);
-    current = current->next;
-  }
-  printf("\n");
+    return 0;
 }
