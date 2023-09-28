854d672ebaba67af1c61211df0a35d106d41d55b
Roshan Limbu <topazkayle@gmail.com>
2023-09-28 15:43:39 +0545

    push func done

--- stack/stack.c
+++ stack/stack.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
-#include<conio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-struct stack {
+#define MAX 10
+typedef struct stack {
+  int top;
+  int data[MAX];
+} Stack;
 
-};
+
+void initialize(Stack *stack){
+  stack->top=-1;
+}
+
+bool isEmpty(Stack *stack){
+  return (stack->top==-1);
+}
+bool isFull(Stack *stack){
+  return (stack->top==MAX-1);
+}
+
+void push(Stack *stack){
+  int n;
+  if(isFull(stack)){
+    printf("Stack overflow");
+  } else {
+    printf("Enter the data to be inserted: ");
+    scanf("%d", &n);
+  }
+  stack->top++;
+  stack->data[stack->top]=n;
+  printf("%d inserted at the top of stack.\n", n);
+
+}
+
+
+int main(){
+  Stack stack;
+  initialize(&stack);
+  int choice;
+
+  do{
+    printf("Enter the choice:\n");
+    scanf("%d", &choice);
+    switch (choice) {
+      case 1:
+        push(&stack);
+        break;
+    }
+  } while(choice!=4);
+  return 0;
+}
