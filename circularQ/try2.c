9b1b2d885f876b79187dd6698570d50e1ad027ff
Roshan Limbu <topazkayle@gmail.com>
2023-09-30 14:33:27 +0545

    enueue done

---
+++ circularQ/try2.c
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stdio.h>
+#define MAX 5
+typedef struct queue {
+  int data[MAX];
+  int front;
+  int rear;
+} Queue;
+
+void initilaize(Queue *queue) {
+  queue->front = -1;
+  queue->rear = -1;
+}
+
+bool isQFull(Queue *queue) { return (queue->rear + 1) % MAX == queue->front; }
+bool isQEmp(Queue *queue) { return (queue->front == -1 && queue->rear == -1); }
+
+void enQ(Queue *queue, int n) {
+  if (isQEmp(queue)) {
+    printf("\nQueue is empty.\n");
+    return;
+  } else {
+    if (isQFull(queue)) {
+      queue->rear = 0;
+    }
+  }
+  queue->rear = (queue->rear + 1) % MAX;
+  queue->data[queue->rear] = n;
+  printf("\n%d inserted in the queue.\n", n);
+}
